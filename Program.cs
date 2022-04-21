7dcf632175f744f66e9c4b7bee451e5a190b2f0a
Svante Pålsson <svantepalsson1987@gmail.com>
2022-04-21 20:03:40 +0200

    Did a build

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 var app = builder.Build();
 
